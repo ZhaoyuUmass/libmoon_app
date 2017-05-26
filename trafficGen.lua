8c64e79c6ae6be082fb589c0c5a36c802a571945
Zhaoyu Gao <gaozy@cs.umass.edu>
2017-05-26 11:13:21 -0400

    add more options

--- trafficGen.lua
+++ trafficGen.lua
@@ -4,12 +4,24 @@ local lm     = require "libmoon"
 local device = require "device"
 local log    = require "log"
 
+-- set addresses here
+local DST_MAC       = nil -- resolved via ARP on GW_IP or DST_IP, can be overriden with a string here
+local PKT_LEN       = 60
+local SRC_IP        = "10.0.0.10"
+local DST_IP        = "10.1.0.10"
+local SRC_PORT_BASE = 1234 -- actual port will be SRC_PORT_BASE * random(NUM_FLOWS)
+local DST_PORT      = 1234
+local NUM_FLOWS     = 1000
+
 -- the configure function is called on startup with a pre-initialized command line parser
 function configure(parser)
   parser:description("Edit the source to modify constants like IPs and ports.")
   parser:argument("dev", "Devices to use."):args("+"):convert(tonumber)
   parser:option("-f --flows", "Number of flows per device."):args(1):convert(tonumber):default(1)
   parser:option("-r --rate", "Transmit rate in Mbit/s per device."):args(1)
+  parser:option("-s --source", "source IP"):args(1)
+  parser:option("-d --dest", "destination IP"):args(1)
+  parser:option("-m --mac", "destination MAC"):args(1)
   parser:flag("-t --tcp", "Use TCP.")
   return parser:parse()
 end
@@ -34,6 +46,5 @@ function master(args,...)
   end
   device.waitForLinks()
   
-  while lm.running() do
-  end
+  
 end
