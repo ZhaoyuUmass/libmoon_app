944b7f2338ad74fe14117e85ce5263747e12c934
Zhaoyu Gao <gaozy@cs.umass.edu>
2017-05-26 09:47:06 -0400

    add log

--- pktgen.lua
+++ pktgen.lua
@@ -4,12 +4,12 @@
 function configure(parser)
   parser:description("Edit the source to modify constants like IPs and ports.")
   parser:argument("dev", "Devices to use."):args("+"):convert(tonumber)
-  parser:option("-t --threads", "Number of threads per device."):args(1):convert(tonumber):default(1)
+  parser:option("-f --flows", "Number of flows per device."):args(1):convert(tonumber):default(1)
   parser:option("-r --rate", "Transmit rate in Mbit/s per device."):args(1)
   parser:flag("-a --arp", "Use ARP.")
   return parser:parse()
 end
 
 function master(args,...)
-
+  log:info("This script is used to generate packets at load of %d Mbit/s with %d flows", args.rate, args.flows)
 end
