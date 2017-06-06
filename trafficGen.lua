c151257390c59a06239da90c0404b3742a8f26f2
Zhaoyu Gao <gaozy@cs.umass.edu>
2017-06-06 12:46:36 -0400

    add latency task

--- trafficGen.lua
+++ trafficGen.lua
@@ -1,5 +1,8 @@
--- Generate TCP or UDP traffic at a rate
+-- Generate TCP or UDP traffic at a fixed rate
+-- NOTE: to run this test, you must make sure that you have at least 2 ports
+-- Usage: sudo ./MoonGen 
 
+local mg     = require "moongen"
 local lm     = require "libmoon"
 local device = require "device"
 local log    = require "log"
@@ -42,12 +45,14 @@ function master(args,...)
   print("DST_IP",DST_IP)
   print("DST_MAC",DST_MAC)
   
-  -- configure devices, we only need a single txQueue to send traffic
+  -- configure devices, we only need a single txQueue to send traffic and another port to send latency traffic
+  -- Note: VF only supports 1 tx and rx queue on agave machines, that's why we hard code the number to 1 here
   local arpQueues = {}
   for i,dev in pairs(args.dev) do
     local dev = device.config{
       port = dev,
-      txQueues = 1
+      txQueues = 1,
+      rxQueues = 1
     }
     args.dev[i] = dev
   end
@@ -55,17 +60,32 @@ function master(args,...)
   device.waitForLinks()
   
   -- print statistics for both tx and rx queues
-  stats.startStatsTask{txDevices = args.dev}
+  stats.startStatsTask{devices = args.dev}
   
   -- start tx tasks
-  for _,dev in pairs(args.dev) do
-    -- initialize a local queue: local is very important here
-    local queue = dev:getTxQueue(0)    
-    -- the software rate limiter always works, but it can only scale up to 5.55Mpps (64b packet) with Intel 82599 NIC on EC2
-    local rateLimiter = limiter:new(queue, pattern, 1 / args.rate * 1000)
-    -- set rate on each device
-    -- queue:setRate(args.rate)
-    lm.startTask("txSlave", queue, DST_MAC, rateLimiter)   
+  for i,dev in pairs(args.dev) do
+    if i == 0 then
+      -- the first port is used for a latency task
+      -- initalize local queues
+      local rxQueue = dev:getRxQueue(0)
+      local txQueue = dev:getTxQueue(0)
+      -- use 1/100 traffic rate for latency task
+      local rateLimiter = limiter:new(queue, pattern, 100/args.rate*1000)
+      lm.startTask("txLatency", txQueue, DST_MAC, rateLimiter)
+      lm.startTask("rxLatency", rxQueue)
+    else
+      -- the rest of the queue is used for sending traffic
+      
+      -- initialize a local queue: local is very important here
+      local queue = dev:getTxQueue(0)    
+      -- the software rate limiter always works, but it can only scale up to 5.55Mpps (64b packet) with Intel 82599 NIC on EC2
+      local rateLimiter = limiter:new(queue, pattern, 1 / args.rate * 1000)
+      --[[ this method does not work with VF
+       set rate on each device
+       queue:setRate(args.rate)
+      ]]--
+      lm.startTask("txSlave", queue, DST_MAC, rateLimiter)   
+    end
   end
   lm.waitForTasks()
 end
@@ -104,6 +124,92 @@ function txSlave(queue, dstMac, rateLimiter)
   end
 end
 
+function txLatency(queue, dstMac, limiter)
+  local mem = memory.createMemPool(function(buf)
+    -- just to use the default filter here
+    -- you can use whatever packet type you want
+    buf:getUdpPacket():fill{
+      -- this is a right setting for server and client on different hosts
+      ethSrc = queue, -- MAC of the tx device
+      ethDst = dstMac,
+      pktLength = PKT_SIZE   
+    }
+  end)
+  mg.sleepMillis(1000) -- ensure that the load task is running
+  local bufs = mem:bufArray()
+  local j = 0
+  local ctr = stats:newDevTxCounter("Load Traffic", queue.dev, "plain")
+  local tm_sent = {}
+  
+  while j < NUM_PKTS and mg.running() do
+    bufs:alloc(PKT_SIZE)
+    for i, buf in ipairs(bufs) do
+      -- packet framework allows simple access to fields in complex protocol stacks
+      local pkt = buf:getUdpPacket()
+      pkt.udp:setSrcPort(SRC_PORT_BASE + math.random(0, NUM_FLOWS - 1))
+      local tm = j*NUM_PKTS+i -- mg:getCycles()
+      -- print("current cycle:",tm)
+      pkt.payload.uint64[0] = tm
+      tm_sent[#tm_sent+1] = tm
+      -- print("payload:",tonumber(pkt.payload.uint64[0]))
+    end
+    bufs:offloadUdpChecksums()
+    -- queue:sendWithTimestamp(bufs)
+    -- queue:send(bufs)
+    rateLimiter:send(bufs)
+    --rateLimiter:wait()
+    --rateLimiter:reset()
+    j = j + 1
+    ctr:update()
+  end
+end
+
+function rxLatency(rxQueue)
+  local tscFreq = mg.getCyclesFrequency()
+  print("tscFreq",tscFreq)
+  
+  local tm_rcvd = {}
+  
+  -- use whatever filter appropriate for your packet type
+  -- queue:filterUdpTimestamps()
+  local ctr = stats:newDevRxCounter("Received Traffic", rxQueue.dev, "plain")
+  local bufs = memory.bufArray()
+  while mg.running() do
+    local rx = rxQueue:tryRecv(bufs, 100)
+    for i = 1, rx do
+      local pkt = bufs[i]:getUdpPacket()
+      -- local dst = pkt.eth:getDst()
+      -- local src = pkt.eth:getSrc()
+      -- local rxTs = bufs[i].udata64
+      -- local txTs = bufs[i]:getSoftwareTxTimestamp()
+      -- print("received a packet with payload", tonumber(pkt.payload.uint64[0]))
+      
+      local rxTs = pkt.payload.uint64[0]
+      tm_rcvd[#tm_rcvd+1] = tonumber(rxTs)
+      
+      -- print("received a packet", rxTs, txTs, tonumber(rxTs - txTs) / tscFreq * 10^9)      
+      ctr:update()
+    end
+    --[[
+    local numPkts = queue:recvWithTimestamps(bufs)
+    for i = 1, numPkts do
+      local rxTs = bufs[i].udata64
+      local txTs = bufs[i]:getSoftwareTxTimestamp()
+      print("received a packet",rxTs, txTs, tonumber(rxTs - txTs) / tscFreq * 10^9)
+      results[#results + 1] = tonumber(rxTs - txTs) / tscFreq * 10^9 -- to nanoseconds
+      ctr:update()
+    end
+    bufs:free(numPkts)
+    ]]--
+  end
+  ctr:finalize()
+  
+  local f = io.open("rcvd.txt", "w+")
+  for i, v in ipairs(tm_rcvd) do
+    f:write(tostring(v) .. "\n")
+  end
+  f:close() 
+end
 
 
 
