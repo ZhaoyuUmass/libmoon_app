cfa752e9ecd4c0fb7a15f6f55c2625f6e08c57a0
Zhaoyu Gao <gaozy@cs.umass.edu>
2017-06-14 10:29:49 -0400

    update config file

--- dpdk-config.lua
+++ dpdk-config.lua
@@ -13,9 +13,10 @@ DPDKConfig {
   -- arbitrary DPDK command line options
   -- the following configuration allows multiple DPDK instances (use together with pciWhitelist)
   -- cf. http://dpdk.org/doc/guides/prog_guide/multi_proc_support.html#running-multiple-independent-dpdk-applications
-  --cli = {
-  --  "--file-prefix", "m1",
+  cli = {
+    "--file-prefix", "m2",
   --  "--socket-mem", "512,512",
-  --}
+    "-m", "512"
+  }
 
 }
