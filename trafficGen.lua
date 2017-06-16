c08fccb02420879611c69977324d29a7f586d95b
Zhaoyu Gao <gaozy@cs.umass.edu>
2017-06-16 12:03:02 -0400

    update stats

--- trafficGen.lua
+++ trafficGen.lua
@@ -191,10 +191,10 @@ function rxLatency(rxQueue)
       local rxTs = pkt.payload.uint64[0]
       
       -- tm_rcvd[#tm_rcvd+1] = rxTs
-      print("received", rxTs)
+      -- print("received", rxTs)
       -- print("received a packet", rxTs, txTs, tonumber(rxTs - txTs) / tscFreq * 10^9)      
       -- ctr:update()
-      -- ctr:countPacket(buf)
+      ctr:countPacket(pkt)
     end
     ctr:update()
     bufs:freeAll()
