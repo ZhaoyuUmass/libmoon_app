dd9b22382f6bc468075d5e3be599da6c2c3961e4
Zhaoyu Gao <gaozy@cs.umass.edu>
2017-06-16 16:01:59 -0400

    instrument

--- trafficGen.lua
+++ trafficGen.lua
@@ -136,7 +136,7 @@ function txSlave(queue, dstMac, rateLimiter, numFlows)
   while lm.running() do -- check if Ctrl+c was pressed
     -- this actually allocates some buffers from the mempool the array is associated with
     -- this has to be repeated for each send because sending is asynchronous, we cannot reuse the old buffers here
-    bufs:alloc()
+    bufs:alloc(PKT_LEN)
     for i, buf in ipairs(bufs) do
       -- packet framework allows simple access to fields in complex protocol stacks      
       --[[
