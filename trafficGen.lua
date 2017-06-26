0b3c9506a2f4fff7a548f4702231c9e0660f14e8
Zhaoyu Gao <gaozy@cs.umass.edu>
2017-06-26 16:04:26 -0400

    debug

--- trafficGen.lua
+++ trafficGen.lua
@@ -100,7 +100,7 @@ function master(args, ...)
   -- start tx tasks
   for i,dev in pairs(args.dev) do
     -- initialize a local queue: local is very important here
-    local queue = dev:getTxQueue(1)    
+    local queue = dev:getTxQueue(0)    
     -- the software rate limiter always works, but it can only scale up to 5.55Mpps (64b packet) with Intel 82599 NIC on EC2
     local rateLimiter = limiter:new(queue, PATTERN, 1 / args.rate * 1000)
     if DST_MAC then
