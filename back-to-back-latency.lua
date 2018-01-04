c2fe4413d07f329e12c3b2a54c3e68bc98f75dba
Zhaoyu Gao <gaozy@cs.umass.edu>
2018-01-04 14:30:11 -0500

    update back to back latency script

--- back-to-back-latency.lua
+++ back-to-back-latency.lua
@@ -74,7 +74,7 @@ function back2backLatency(txDev, rxDev, dstMac)
     -- wait for packet: no time out until packet returns
     local rx = rxQueue:tryRecv(buf_rcvd)
     local elapsed = lm:getCycles() - begin
-    print("latency:",elapsed)
+    print("latency:",elapsed/tscFreq)
     buf_rcvd:free(rx)
     j = j+1  
   end
